99a51d4530fe47a57cb20cbff97328aa1caa2312
luckydeveloper91 <luckydraw.admin@nonecode.site>
2025-07-16 23:49:37 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'  // default, but make it explicit
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
